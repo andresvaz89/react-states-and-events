a33cce95abb7570373f11839cd40377ab7e6869f
Andres Vazquez <andres.vazquez.martin@gmail.com>
2023-10-02 22:37:05 +0200

    counter and onChange dark mode

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Counter from './components/Counter';
+
+import React, { useState } from 'react';
+
+function App() {
+  const [theme, setTheme] = useState('light');
+
+  return (
+    <div className={'App ' + theme}>
+      <h1>React - state and events</h1>
+      <Counter />
+      <select onChange={(event) => setTheme(event.target.value)}>
+        <option value="light"> Light </option>
+        <option value="dark"> Dark </option>
+      </select>
+    </div>
+  );
+}
+
+export default App;
